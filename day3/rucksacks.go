ae50015696d9deea55eaa3a60dec448ea12f475b
Kommander B <daniel.berlemont@skaleet.com>
2022-12-05 23:09:42 +0100

    implemented day3 puzzle 2

--- day3/rucksacks.go
+++ day3/rucksacks.go
@@ -8,20 +8,31 @@ import (
 
 const PriorityMap = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func splitRucksackCompartments(rucksackContent string) ([]string, int) {
+func splitRucksackCompartments(rucksackContent string) []string {
 	rucksackCompartmentSize := len(rucksackContent) / 2
 
 	return []string{
 		rucksackContent[0:rucksackCompartmentSize],
 		rucksackContent[rucksackCompartmentSize:],
-	}, rucksackCompartmentSize
+	}
 }
 
-func getCommonItemType(rucksackCompartments []string, rucksackCompartmentSize int) string {
+func getCommonItemType(rucksackCompartments []string) string {
 	var commonItemType = ""
-	for index := 0; index < rucksackCompartmentSize; index++ {
-		if strings.Contains(rucksackCompartments[1], rucksackCompartments[0][index:index+1]) {
-			commonItemType = rucksackCompartments[0][index : index+1]
+	for index := 0; index < len(rucksackCompartments[0]); index++ {
+		search := rucksackCompartments[0][index : index+1]
+
+		foundCount := 0
+		for rucksackIndex := 0; rucksackIndex < len(rucksackCompartments); rucksackIndex++ {
+			if strings.Contains(rucksackCompartments[rucksackIndex], search) {
+				foundCount = foundCount + 1
+			} else {
+				break
+			}
+		}
+
+		if foundCount == len(rucksackCompartments) {
+			commonItemType = search
 		}
 	}
 
@@ -39,9 +50,50 @@ func getItemTypePriority(itemType string) int {
 }
 
 func getRucksackPriorityValue(rucksackNumber int, rucksack string) int {
-	rucksackCompartments, rucksackCompartmentSize := splitRucksackCompartments(rucksack)
+	rucksackCompartments := splitRucksackCompartments(rucksack)
 
-	commonItemType := getCommonItemType(rucksackCompartments, rucksackCompartmentSize)
+	commonItemType := getCommonItemType(rucksackCompartments)
+
+	return getItemTypePriority(commonItemType)
+}
+
+func orderRucksacks(rucksacks []string) []string {
+	permutated := false
+	for {
+		permutated = false
+		for index := 0; index < len(rucksacks); index++ {
+			if index+1 < len(rucksacks) {
+				if len(rucksacks[index]) > len(rucksacks[index+1]) {
+					tempRucksack := rucksacks[index+1]
+					rucksacks[index+1] = rucksacks[index]
+					rucksacks[index] = tempRucksack
+					permutated = true
+				}
+			} else {
+				if len(rucksacks[index]) < len(rucksacks[index-1]) {
+					tempRucksack := rucksacks[index-1]
+					rucksacks[index-1] = rucksacks[index]
+					rucksacks[index] = tempRucksack
+					permutated = true
+				}
+			}
+		}
+
+		if !permutated {
+			break
+		}
+	}
+
+	return rucksacks
+}
+
+func getRucksacksGroupPriorityValue(rucksacks []string) int {
+
+	orderedRucksacks := orderRucksacks(rucksacks)
+
+	fmt.Println("ordered?", orderedRucksacks)
+
+	commonItemType := getCommonItemType(orderedRucksacks)
 
 	return getItemTypePriority(commonItemType)
 }
@@ -50,15 +102,21 @@ func main() {
 	inputFileLines := utils.IngestInputFile("./input.txt")
 
 	var prioritiesAccumulator = 0
+	var rucksacksGroup []string
 	for lineIndex := 0; lineIndex < len(inputFileLines); lineIndex++ {
 		if len(inputFileLines[lineIndex]) == 0 {
 			continue
 		}
 
-		prioritiesAccumulator = prioritiesAccumulator + getRucksackPriorityValue(lineIndex, inputFileLines[lineIndex])
+		rucksacksGroup = append(rucksacksGroup, inputFileLines[lineIndex])
+
+		if len(rucksacksGroup) == 3 {
+			prioritiesAccumulator = prioritiesAccumulator + getRucksacksGroupPriorityValue(rucksacksGroup)
+
+			rucksacksGroup = []string{}
+		}
+
 	}
 
 	fmt.Println("Sum of priorities:", prioritiesAccumulator)
 }
-
-// Rucksack => 2 compartments
